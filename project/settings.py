ecf73f76c96f524724906aae3c52505f9ff16ab1
Ahme460 <ahmeoon1234@gmail.com>
2024-08-03 17:20:01 +0300

    new

--- project/settings.py
+++ project/settings.py
@@ -50,15 +50,16 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
 
 # CORS settings
 CORS_ALLOW_ALL_ORIGINS = True
