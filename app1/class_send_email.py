f5f504f8017e672701c6933c30a034a82e69b854
Ahme460 <ahmeoon1234@gmail.com>
2024-09-01 19:35:35 +0300

    up

--- app1/class_send_email.py
+++ app1/class_send_email.py
@@ -4,36 +4,34 @@ from django.utils.html import strip_tags
 from django.core.files.base import ContentFile
 from . models import Customer_user
 from django.conf import settings
-class Sender_mail:
-    def __init__(self,subject,content) -> None:
-        self.subject=subject
-        self.content=content
+class SenderMail:
+    def __init__(self, subject, content) -> None:
+        self.subject = subject
+        self.content = content
         
-    def send_mail(self,email):
+    def send_mail(self, emails):
         context = {
-                'title': self.subject,
-                'body': self.content,
-            }
+            'title': self.subject,
+            'body': self.content,
+        }
 
-            # Render the HTML content using the template
+        # Render the HTML content using the template
         html_content = render_to_string('email_template.html', context)
         text_content = strip_tags(html_content)
 
-        # Get all user email addresses
-        recipients = email
+        # Ensure emails is a list
+        if isinstance(emails, str):
+            emails = [emails]
 
         # Ensure there are recipients
-        if recipients:
+        if emails:
             # Send the email to all users
             msg = EmailMultiAlternatives(
-                self.subject,  # Subject
-                self.content,
-                #text_content,  # Plain text content
-                settings.DEFAULT_FROM_EMAIL,  # From email
-                recipients  # List of all user emails
+                subject=self.subject,                # Subject
+                body=text_content,                  # Plain text content
+                from_email=settings.DEFAULT_FROM_EMAIL, # From email
+                to=emails                           # List of all user emails
             )
             msg.attach_alternative(html_content, "text/html")
             msg.send()
-            
-            
             
