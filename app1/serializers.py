f7e4c4a1d8a451229a72148348351e3e9b37b789
Ahme460 <ahmeoon1234@gmail.com>
2024-09-15 12:59:29 +0300

    up

--- app1/serializers.py
+++ app1/serializers.py
@@ -216,8 +216,8 @@ class CartSer(serializers.ModelSerializer):
         model = CartModel
         fields = "__all__"
         
-    def get_total_price(self, obj):
-        return obj.total_price
+    #def get_total_price(self, obj):
+        #return obj.total_price
         
    #products=ProductSerializer(many=True)
     #total_price = serializers.SerializerMethodField(read_only=True)
