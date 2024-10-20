96640387c3c571a6a6f0167c41cd3f9456e6be86
Ahme460 <ahmeoon1234@gmail.com>
2024-10-20 03:36:14 +0300

    up

--- project/settings.py
+++ project/settings.py
@@ -118,12 +118,27 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bantagy',  # اسم قاعدة البيانات
+        'USER': 'ahmed',  # اسم المستخدم
+        'PASSWORD': 'ahmed',  # كلمة المرور
+        'HOST': 'localhost',  # قاعدة البيانات على نفس السيرفر
+        'PORT': '5432',  # المنفذ الافتراضي لـ PostgreSQL
     }
 }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
