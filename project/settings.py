1ecadcb2b80759ab1db2d0ef3d9f0a2828087dab
Ahme460 <ahmeoon1234@gmail.com>
2024-10-29 05:27:00 +0300

    up

--- project/settings.py
+++ project/settings.py
@@ -224,7 +224,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # EMAIL_HOST_USER = "bentagwy2121@gmail.com"
 # EMAIL_HOST_PASSWORD = "llux mtlk fvot yekd" 
 # FRONTEND_URL=''
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 
 EMAIL_USE_TLS = True
